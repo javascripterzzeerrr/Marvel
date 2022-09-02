dde56c62546adba2473173dd48dd87e106ca9745
Nikita <stroganov_n777@mail.ru>
2022-09-02 12:37:42 +0300

    Lifecycle methods

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,90 @@
+import { Component } from 'react';
 import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/Spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+
+class CharList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        data: [],
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    onCharLoaded = (data) => {
+        this.setState({
+            data,
+            loading: false
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            error: !this.state.error,
+        })
+    }
+
+    updateItems = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    // lifecycle methods
+    componentDidMount() {
+        this.updateItems();
+    }
+
+    render() {
+
+        const {data, loading, error} = this.state;
+        const errorMsg = error ? <ErrorMessage /> : null;
+        const spinner = (loading && !error) ? <Spinner /> : null;
+        const content = !(loading || error) ? <Item char={data} onCharSelected={this.props.onCharSelected} /> : null;
+
+        return (
+            <div className="char__list">
+                {errorMsg}
+                {spinner}
+                <ul className="char__grid">
+                    {content}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+const Item = ({char, onCharSelected}) => {
+    let flag = false;
+
+    return char.map(item => {
+        const {thumbnail, name} = item;
+        flag = false;
+        if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            flag = true;
+        }
+
+        return (
+            <li
+                className="char__item"
+                key={item.id}
+                onClick={() => onCharSelected(item.id)}>
+                <img src={thumbnail} alt="abyss" style={flag ? {objectFit: 'contain'} : {objectFit: 'cover'}}/>
+                <div className="char__name">{name}</div>
+            </li>
+        );
+    })
 }
 
 export default CharList;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,4 @@
-
+/* Class for working with API */
 
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
@@ -14,25 +14,30 @@ class MarvelService {
         return await res.json();
     }
 
+    // Getting all characters from the server
     getAllCharacters = async () => {
         const url = `${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`;
         const res = await this.getResource(url);
         return res.data.results.map(this._transformCharacter);
     }
 
+    // Getting one character from the server
     getCharacter = async (id) => {
         const url = `${this._apiBase}characters/${id}?apikey=${this._apiKey}`;
         const res = await this.getResource(url);
         return this._transformCharacter(res.data.results[0]);
     }
 
+    // transform data which comes from the server
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: (char.description) ? (char.description.slice(0,210) + '...') : "The content is empty",
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
